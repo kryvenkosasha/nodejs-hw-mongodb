a01427a2e4828f7cc57a8422e9593b71e17a3912
Olena Kryvenko <sashakryvenko2006@gmail.com>
2024-07-07 22:15:28 +0200

    Update sendMail.js

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,15 +1,13 @@
 import nodemailer from 'nodemailer';
-// import { SMTP } from '../constants/contacts.js';
-// import { env } from './env.js';
-import dotenv from 'dotenv';
-dotenv.config();
+import { SMTP } from '../constants/contacts.js';
+import { env } from './env.js';
 
 const transporter = nodemailer.createTransport({
-  host: process.env['SMTP_HOST'],
-  port: process.env['SMTP_PORT'],
+  host: env(SMTP.SMTP_HOST),
+  port: Number(env(SMTP.SMTP_PORT)),
   auth: {
-    user: process.env['SMTP_USER'],
-    pass: process.env['SMTP_PASSWORD'],
+    user: env(SMTP.SMTP_USER),
+    pass: env(SMTP.SMTP_PASSWORD),
   },
 });
 ///
