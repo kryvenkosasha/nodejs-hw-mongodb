70c377ff37f1b085c4ce92355dc22538ec3da48e
Olena Kryvenko <sashakryvenko2006@gmail.com>
2024-07-08 12:50:53 +0200

    update

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -9,7 +9,10 @@ import {
 
 import { validateBody } from '../middlewares/validateBody.js';
 import { isValidId } from '../middlewares/validateMongoId.js';
-import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { authenticate } from '../middlewares/authenticate.js';
 
@@ -19,34 +22,25 @@ const router = Router();
 
 router.use(authenticate);
 
-
 router.get('/', ctrlWrapper(getContactsController));
 
-router.get(
-  '/:contactId',
-  isValidId,
-  ctrlWrapper(getContactByIDController),
-);
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIDController));
 
 router.post(
   '/',
-  ctrlWrapper(createContactController),
   upload.single('photo'),
+  ctrlWrapper(createContactController),
   validateBody(createContactSchema),
 );
 
 router.patch(
   '/:contactId',
   isValidId,
-  ctrlWrapper(patchContactController),
   upload.single('photo'),
+  ctrlWrapper(patchContactController),
   validateBody(updateContactSchema),
 );
 
-router.delete(
-  '/:contactId',
-  isValidId,
-  ctrlWrapper(deleteContactController),
-);
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
